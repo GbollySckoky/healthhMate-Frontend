97de8bb66987b7ccca78c3f59bb5dda6679e08f6
Gbolly <gbolahancoker99@gmail.com>
2025-09-30 18:55:14 +0100

    refactoring the code

--- apps/doctor/app/(overview)/layout.tsx
+++ apps/doctor/app/(overview)/layout.tsx
@@ -1,4 +1,3 @@
-// import Provider from "@/components/provider/Provider";
 import Sidebar from "@/components/ui/Sidebar";
 import Header from "@/components/ui/Header";
 
@@ -10,12 +9,10 @@ export default function OverviewLayout({
   return (
       <div className=" flex">
         <Sidebar />
-        {/* <Provider> */}
           <div className="flex-1  md:ml-[250px] ml-0">
             <Header />
             <div>{children}</div>
           </div>
-        {/* </Provider> */}
       </div>
   );
 }
