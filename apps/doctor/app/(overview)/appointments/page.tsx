183c88bf73ecf1aad6aa1daa01ceac8b776e39b4
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-01-04 15:11:51 +0100

    debuging

--- apps/doctor/app/(overview)/appointments/page.tsx
+++ apps/doctor/app/(overview)/appointments/page.tsx
@@ -11,12 +11,14 @@ import { Doctor } from '@/lib/constant/service'
 
 
 const Page = () => {
-  // const {data, isLoading, error, isError} = useQuery({
-  //   queryKey: ['getAppointment'],
-  //   queryFn: () => Doctor.getAppointment()
-  // })
+  const {data, isLoading, error, isError} = useQuery({
+    queryKey: ['getAppointment'],
+    queryFn: () => Doctor.getAppointment()
+  })
 
-  // console.log(data)
+  console.log(data)
+  console.log(isLoading)
+  console.log(error)
   return (
     <PageWrapper>
         <Tabs defaultValue="allAppointment" >
