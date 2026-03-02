5b769bd87c5b12fc07a5ad0ca6dc44502e218a01
Gbolahan Coker <gbolahancoker99@gmail.com>
2026-03-02 10:56:26 +0100

    okau

--- apps/doctor/app/loading.js
+++ apps/doctor/app/loading.js
@@ -1,7 +1,7 @@
 export default function loading() {
     return (
       <div className="flex items-center justify-center">
-        <h1 className="flex items-center justify-center h-[60vh]">Loading....</h1>
+        <h1 className="flex items-center justify-center h-screen bg-grey-100 ">Loading....</h1>
       </div>
     );
   }
